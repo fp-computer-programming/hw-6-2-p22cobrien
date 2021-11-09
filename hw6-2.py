11c02514c8d54c18cf015113603f5563d59dc0a1
p22cobrien <p22cobrien@fairfieldprep.org>
2021-11-09 10:13:56 -0500

    add hw6-2.py

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: CMOB 11/9/2021
+
+# Question 1
+lst1 = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(lst1))
+
+# Question 3
+print(lst1[2])
+
+# Question 4
+print(5 not in lst1)
+
+# Question 5
+print(10 not in lst1)
+
+# Question 6
+name_lst = ['Colin', 'Ian', 'Mataeo', 'Micheal']
+
+# Question 7
+print(name_lst[0])
+
+# Question 8
+print(name_lst * 3)
+
+# Question 9
+lst = lst1 + name_lst
+
+# Question 10
+print(len(lst))
